e58302a00e1bb72e19a42b2d574dfc9d9132d58d
henricker <henriquevieira@alu.ufc.br>
2022-10-21 09:22:43 -0300

    test: ensure text processor convert each line of data to json object

---
+++ tests/unit/text-processor-fluent-api.test.js
@@ -0,0 +1,50 @@
+const { expect } = require("chai");
+const { TextProcessorFluentAPI } = require("../../src/text-processor-fluent-api");
+const valid = require('../mocks/valid');
+
+
+describe('TextProcessorFluentAPI', () => {
+    let textProcessorFluentAPI;
+    beforeEach(() => {
+        textProcessorFluentAPI = new TextProcessorFluentAPI(valid);
+    });
+
+    it('Should return content', () => {
+        const expected = valid;
+        const result = textProcessorFluentAPI.build();
+        expect(result).to.be.deep.equal(expected);
+    })
+
+    it('Should convert text csv to array of objects', () => {
+        const data = textProcessorFluentAPI.convertRowsToObject().build();
+
+        const expected = [
+        {
+            'título': 'Projeto de lei 584/2016',
+            link: 'http://www.al.sp.gov.br/propositura?id=1322563',
+            autor: 'Jorge Wilson Xerife do Consumidor',
+            etapa: 'PAUTA',
+            ementa: 'Dispõe sobre a inclusão de cláusula nos contratos de adesão aos serviços de telefonia fixa, de telefonia móvel e de banda larga móvel, e dá outras providências.',
+            indexadoresnorma: 'CONTRATO, OBRIGATORIEDADE, CLÁUSULA, SERVIÇO, TELEFONIA MÓVEL, TELEFONIA FIXA, PRAZO, INCLUSÃO, RESCISÃO CONTRATUAL, LIBERAÇÃO'
+        },
+        {
+            'título': 'Projeto de lei 580/2016',
+            link: 'http://www.al.sp.gov.br/propositura?id=1323286',
+            autor: 'Marcia Lia',
+            etapa: 'PAUTA',
+            ementa: 'Estabelece normas gerais para a realização de Concurso Público pela Administração Pública Direta e Indireta do Estado.',
+            indexadoresnorma: 'NORMAS, REALIZAÇÃO, CONCURSO PÚBLICO ESTADUAL, ESTADO DE SÃO PAULO, ADMINISTRAÇÃO PÚBLICA DIRETA E INDIRETA'
+        },
+        {
+            'título': 'Projeto de lei 545/2016',
+            link: 'http://www.al.sp.gov.br/propositura?id=1322832',
+            autor: 'Roberto Morais, Itamar Borges',
+            etapa: 'PAUTA',
+            ementa: 'Altera a Lei nº 13.550, de 2009, que dispõe sobre a utilização e proteção da vegetação nativa do Bioma Cerrado no Estado de São Paulo.',
+            indexadoresnorma: ''
+        }
+        ]
+
+        expect(data).to.be.deep.equal(expected);
+    })
+});
